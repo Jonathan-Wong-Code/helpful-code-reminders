24ed850ad29cf8941f6550c51a6b021caf9fe360
Jonathan Wong <jonathan.wong@applydigital.co>
2020-10-29 12:32:15 -0400

    update

--- typescript/useStateUpdater.tsx
+++ typescript/useStateUpdater.tsx
@@ -1,1 +1,8 @@
+  // Any state updater that uses prevState must be typed out as if the updater function is the argument.
+  
   setImagePreviews: (fn: (prevState: ImagePreview) => ImagePreview) => void;
+
+   setImagePreviews((prevState: ImagePreview) => ({
+        ...prevState,
+        selfie: URL.createObjectURL(file),
+      }));
